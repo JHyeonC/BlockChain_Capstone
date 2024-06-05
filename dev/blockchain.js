dae99ab8a1423e1cee1c753276a9f5081a4b5a11
choejonghyeon <choijonghyen@gmail.com>
2024-06-05 23:05:54 +0900

    [BC-002] 잔고 부족 Exception 처리

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -36,6 +36,9 @@ Blockchain.prototype.getLastBlock = function() {
 
 
 Blockchain.prototype.createNewTransaction = function(amount, sender, recipient) {
+	const senderBalance = this.getAddressData(sender).addressBalance;
+	if(amount > senderBalance) throw new Exception("잔고가 부족하여 거래할 수 없습니다.");
+
 	const newTransaction = {
 		amount: amount,
 		sender: sender,
